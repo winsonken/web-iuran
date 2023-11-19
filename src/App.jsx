f2c845e8fe5dbde19f82fc105755a3a18716630d
winsonken <winson.kennedy222@gmail.com>
2023-11-19 16:35:28 +0700

    install react & tailwind

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+
+function App() {
+  
+  return (
+    <>
+      <div>
+        <p className="bg-blue-100">Hello!</p>
+      </div>
+    </>
+  )
+}
+
+export default App
